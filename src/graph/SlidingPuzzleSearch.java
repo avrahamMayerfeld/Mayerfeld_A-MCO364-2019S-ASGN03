6d6d2873fd630eb42b1536a2fe004e92f1844791
Your Name <you@example.com>
2019-04-17 14:08:53 -0400

    reorganized the code. Iterative Deepening DFS

--- src/graph/SlidingPuzzleSearch.java
+++ src/graph/SlidingPuzzleSearch.java
@@ -4,51 +4,59 @@ import java.util.HashSet;
 import java.util.Stack;
 
 public class SlidingPuzzleSearch implements IPuzzleSearch {
-
 	private HashSet<Integer> set = new HashSet<Integer>();
 	private Stack<INode> stack = new Stack<INode>();
+	private boolean solutionFound;
 	private INode currentNode;
-	private int iterativeDepth = 0;
+	private int bound = 0;//for iterative deepening
 	@Override
 	public void search(INode searchNode) 
 	{   
-		currentNode = searchNode;
-		while(!currentNode.isSolution())
+		int timesDidDFS = 0;
+		while(!solutionFound)
 		{	
 			set.clear();
-			iterativeDepth ++;
-			performSearch();
+			bound ++;
+			currentNode = searchNode;
+			stack.push(currentNode);
+			doBoundedDFS();
+			timesDidDFS++;
+			if(timesDidDFS > 50)//prevent infinite loop in unsolvable puzzles
+			{
+				System.out.println("This puzzle is unsolvable.");
+				break;
+			}
 		}
-		
+		if(currentNode.isSolution()) 
+		{
+			System.out.println("Solution found on level "+ currentNode.getLevel());
+			System.out.println();
+			System.out.println(currentNode.toString());
+		}
 	}
 	
-	public void performSearch() 
-	{
-		stack.push(currentNode);
-		set.add(currentNode.hashCode());
+	private void doBoundedDFS() 
+	{   
 		while (!stack.isEmpty())
-	    {   
+		{   
+			set.add(currentNode.hashCode());
 			System.out.println("Searching on level " + currentNode.getLevel());
-	    	if(currentNode.isSolution())
-	    	{
-	    		System.out.println("Solution found on level "+ currentNode.getLevel());
-	    		System.out.println();
-	    		System.out.println(currentNode.toString());
-	    		break;// we are only searching for the first solution found
-	    	}
-	    	
-	    	if(currentNode.getNextNodes() != null && currentNode.getLevel() <= iterativeDepth)
+			if(currentNode.isSolution())
+			{
+				solutionFound = true;
+				break;
+			}
+			if(currentNode.getNextNodes() != null && currentNode.getLevel() <= bound)
 	    	{
 			    for(INode child : currentNode.getNextNodes()) 
 			    {
 				   	if(!set.contains(child.hashCode()))
 				   	{
 				   		stack.push(child);
-				    }
-			    }
+				   	}
+				}
 	    	}
 	    	currentNode = stack.pop();
-	    	set.add(currentNode.hashCode());
 	    }
 	}
 }
